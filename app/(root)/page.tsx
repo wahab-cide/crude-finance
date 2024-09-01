ab6d9cf68850b5eb6f34496f07a5e5916cad58c5
wahab-cide <awca20278@gmail.com>
2024-09-01 13:15:09 -0400

    add initial header components and styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Wahab'};
+
   return (
-    <div>Home</div>
+    <section className="home bg-black">
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox type = "greeting" 
+          title = "Welcome" 
+          user={ loggedIn?.firstName || 'Guest'} 
+          subtext = "Manage your accounts, transactions, and subscriptions in one place"
+          />
+        </header>
+       <TotalBalanceBox 
+        accounts = {[]}
+        totalBanks = {1}
+        totalCurrentBalance = {1705.24}
+        />
+      </div>
+    </section>
   )
 }
 
---
+++ components/ui/HeaderBox.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HeaderBox = ({ type =  'title', title, subtext, 
+    user}: HeaderBoxProps) => {
+  return (
+    <div>
+        <h1 className='header-box-title'>
+            {title}
+            {type === 'greeting' && (
+                <span className='text-gray-500'>
+                    &nbsp;{user}
+                </span>
+            )}
+        </h1>
+        <p className='header-box-subtext'>{subtext}.</p>
+    </div>
+  )
+}
+
+export default HeaderBox
