30003f8be89974d18c04e1be84d7aaf5e70e70af
wahab-cide <awca20278@gmail.com>
2024-09-05 12:03:39 -0400

    fixed sign-up/sign-in form authentication

--- components/ui/CustomInput.tsx
+++ components/ui/CustomInput.tsx
@@ -19,37 +19,37 @@ import { authFormSchema } from '@/lib/utils'
 
 const formSchema = authFormSchema('sign-up')
 
-interface CustomInput{
-  control: Control<z.infer<typeof formSchema>>, 
-  name: FieldPath<z.infer<typeof formSchema>> ;
-  label: string;
-  placeholder: string;
+interface CustomInput {
+  control: Control<z.infer<typeof formSchema>>,
+  name: FieldPath<z.infer<typeof formSchema>>,
+  label: string,
+  placeholder: string
 }
 
-const CustomInput = ({control, name, label, placeholder}: CustomInput) => {
+const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
   return (
-        <FormField
-                control={control}
-                name={name}
-                render={({ field }) => (
-                  <div className='form-item'>
-                    <FormLabel className='form-label'>
-                      {label}
-                    </FormLabel>
-                    <div className='flex flex-col w-full'>
-                      <FormControl>
-                        <Input 
-                        placeholder={placeholder}
-                        className='input-class'
-                        type = {name === 'password' ? "password" : 'text'}
-                        {...field}/>
-                      </FormControl>
-                      <FormMessage className='form-message mt-2'
-                      />
-                    </div>
-                  </div>
-                )}
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">
+            {label}
+          </FormLabel>
+          <div className="flex w-full flex-col">
+            <FormControl>
+              <Input 
+                placeholder={placeholder}
+                className="input-class"
+                type={name === 'password' ? 'password' : 'text'}
+                {...field}
               />
+            </FormControl>
+            <FormMessage className="form-message mt-2" />
+          </div>
+        </div>
+      )}
+    />
   )
 }
 
--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -17,11 +17,11 @@ const Footer = ({user, type='desktop'}: FooterProps) => {
   return (
     <footer className='footer'>
         <div className={type === 'mobile'? 'footer_name-mobile' : 'footer_name'}>
-            <p className='text-xl font-bold text-gray-500'>{user?.name[0]}</p>
+            <p className='text-xl font-bold text-gray-500'>{user?.firstName[0]}</p>
         </div>
 
         <div className={type === 'mobile'? 'email-mobile' : 'email'}>
-            <h1 className='text-14 truncate font-normal text-gray-500'>{user?.name}</h1>
+            <h1 className='text-14 truncate font-normal text-gray-500'>{user?.firstName}</h1>
             <p className='text-14 truncate font-normal text-gray-25'>{user?.email}</p>
         </div>
         <div className='footer_image' onClick={handleLogOut}>
